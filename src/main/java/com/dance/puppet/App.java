3a91f0e99434d785ab36f2116260bd4e813f9c3c
Chan <czihong@gmail.com>
2013-01-09 15:20:02 +0800

    BootStrap class commit

--- src/main/java/com/dance/puppet/App.java
+++ src/main/java/com/dance/puppet/App.java
@@ -1,13 +1,30 @@
 package com.dance.puppet;
 
+import com.dance.puppet.logger.Logger;
+
+
 /**
  * Hello world!
- *
+ * 
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!");
-    }
+public class App {
+	//static Logger	logger	= Logger.getLogger(App.class);
+	private static final String	CLASSNAME	= App.class.getName();
+	
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+		//logger.debug("This is debug message");
+		//myMethod();
+		//logger.info("this is info message");
+		Logger.logWarnMessage(CLASSNAME, "main",
+				"Error while processing result from the netscalar ");
+	}
+
+	public static void myMethod() {
+		try {
+			throw new Exception("My Exception");
+		} catch (Exception e) {
+			//logger.error("this is an exception", e);
+		}
+	}
 }
---
+++ src/test/java/com/dance/puppet/startup/BoosStrap.java
@@ -0,0 +1,55 @@
+package com.dance.puppet.startup;
+
+import java.io.FileInputStream;
+import java.util.ArrayList;
+import java.util.Properties;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+import org.apache.log4j.Logger;
+
+import com.dance.puppet.processor.MyRunnable;
+import com.dance.puppet.util.FileHelper;
+
+public class BoosStrap {
+	static Logger	logger	= Logger.getLogger(BootStrap.class);
+
+	public static void main(String[] args) {
+		ArrayList<String> list = FileHelper.fileToList("server/localtest.list");
+		String onelineCommond = FileHelper.bashToOneLine("script/test.sh");
+		logger.info("execute the following commond");
+		logger.info(onelineCommond);
+		String user = null;
+		String password = null;
+		Properties prop = new Properties();
+		try {
+			prop.load(new FileInputStream("properties/test.properties"));
+			user = prop.getProperty("USERNAME");
+			password = prop.getProperty("PASSWORD");
+		} catch (Exception e) {
+			System.out.println();
+		}
+
+		String cmd = "";
+		Long beforeTimer = System.currentTimeMillis();
+		logger.info("Before Timer is: " + beforeTimer);
+
+		ExecutorService threadPool = Executors.newFixedThreadPool(50);
+		MyRunnable cmdRunnable = null;
+		for (String host : list) {
+			cmd = onelineCommond;
+			cmdRunnable = new MyRunnable(user, password, host, cmd);
+			threadPool.submit(cmdRunnable);
+		}
+		threadPool.shutdown();
+		try {
+			threadPool.awaitTermination(600, TimeUnit.SECONDS);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		Long afterTimer = System.currentTimeMillis();
+		logger.info("After Timer is: " + afterTimer);
+		logger.info("Total Time eclapse is " + (afterTimer - beforeTimer) / 1000 + " seconds");
+	}
+}
