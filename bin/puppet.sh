71ea09d1a6a7f00e961dcedf7c5ba300fe1eae4a
czihong <czihong@gmail.com>
2013-01-15 12:17:24 +0800

    puppet.sh update

--- bin/puppet.sh
+++ bin/puppet.sh
@@ -1,21 +1,75 @@
 #!/bin/bash
-# -----------------------------------------------------------------------------
-# run puppet
-# 
-# @author Chan Chen
-# -----------------------------------------------------------------------------
+# -u username
+# -p password
+# -s server list
+# -c command or script
 
-java_pid=`ps -ef | grep "puppet" | tr -s ' ' |cut -d ' ' -f 1` 
-
-
-if [[ $java_pid -ne 0 ]]; then 
-echo "Puppet  is already running with - PID:[$java_pid]..."
-sleep 1
-exit 0
+#Help Document Start
+if [[ "$1" -eq "--help ]]; then
+	echo "======================================================================="
+	echo ""
+	echo "puppet -u username -p password -s server_list -c script_text"
+	echo "puppet -u puppet -p puppet -s server/server.list -c script/test.sh"
+	echo ""
+	echo "======================================================================="
 fi
+#Help Document End
 
-echo
-echo Error while starting Puppet on 'uname -n'
-echo
 
-sleep 1
+#Process Check Start
+java_pid=`ps -ef | grep "puppet" | tr -s ' ' |cut -d ' ' -f 3`
+
+echo $java_pid
+
+if [[ "$java_pid" -ne 0 ]]; then
+	echo "Puppet  is already running with - PID:[$java_pid]..."
+  sleep 1
+  exit 0
+fi
+#Process Check End
+
+#Argument Validation Start
+for arg in $@
+do
+	case "$arg" in
+
+  -u )    username_flag=1
+          ;;
+
+  -p )    password_flag=1
+          ;;
+
+  -s )    serverlist_flag=1
+          ;;
+
+  -c )    script_flag=1
+          ;;
+
+  * )     ;;
+  esac
+done
+
+if [[ "$username_flag" -ne 1 ]]; then
+  echo "Username Donot Specified"
+  echo "run 'puppet --help' to see help document"
+  exit 0
+fi
+if [[ "$password_flag" -ne 1 ]]; then
+  echo "Password Donot Specified"
+  echo "run 'puppet --help' to see help document"
+  exit 0
+fi
+if [[ "$serverlist_flag" -ne 1 ]]; then
+  echo "Server List Donot Specified"
+  echo "run 'puppet --help' to see help document"
+  exit 0
+fi
+if [[ "$script_flag" -ne 1 ]]; then
+  echo "Script Text Donot Specified"
+  echo "run 'puppet --help' to see help document"
+  exit 0
+fi
+#Argument Validation End
+
+java -jar puppet.jar $1 $2 $3 $4
+
