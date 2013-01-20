f76bdaf0ee491882a051029c0897d32c9adbd68c
Chan <czihong@gmail.com>
2013-01-20 23:56:56 +0800

    RemoveProcessor Class

--- src/main/java/com/dance/puppet/processor/ProcessorFactory.java
+++ src/main/java/com/dance/puppet/processor/ProcessorFactory.java
@@ -5,9 +5,12 @@ public class ProcessorFactory {
 	public IProcessor createProcessor(String processorType){
 		if(processorType.equalsIgnoreCase("NORMAL")){
 			return new NormalProcessor();
-		}if(processorType.equalsIgnoreCase("ANSWER")){
+		}else if(processorType.equalsIgnoreCase("ANSWER")){
 			return new AnswerProcessor();
+		}else if(processorType.equalsIgnoreCase("REMOVE")){
+			return new RemoveProcessor();
 		}
+		
 		return null;
 	}
 }
