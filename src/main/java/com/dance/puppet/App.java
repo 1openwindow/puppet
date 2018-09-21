0d53cb0534e13bf62b85ffe299d80c81835dbfeb
zihch <zihch@outlook.com>
2018-09-21 08:08:55 +0800

    init

--- src/main/java/com/dance/puppet/App.java
+++ src/main/java/com/dance/puppet/App.java
@@ -8,25 +8,25 @@ import org.slf4j.LoggerFactory;
  * 
  */
 public class App {
-	//static Logger	logger	= Logger.getLogger(App.class);
-	//private static final String	CLASSNAME	= App.class.getName();
-	static Logger logger = LoggerFactory.getLogger(App.class);
-			
+	// static Logger logger = Logger.getLogger(App.class);
+	// private static final String CLASSNAME = App.class.getName();
+	static Logger	logger	= LoggerFactory.getLogger(App.class);
+
 	public static void main(String[] args) {
-		//System.out.println("Hello World! Alpha Submmit. Final Alpha Thinks");
-		//logger.debug("This is debug message");
-		//myMethod();
-		//logger.info("this is info message");
-		//Logger.logWarnMessage(CLASSNAME, "main",
-		//		"Error while processing result from the netscalar ");
-		logger.debug("hello {}", "world" );
+		// System.out.println("Hello World! Alpha Submmit. Final Alpha Thinks");
+		// logger.debug("This is debug message");
+		// myMethod();
+		// logger.info("this is info message");
+		// Logger.logWarnMessage(CLASSNAME, "main",
+		// "Error while processing result from the netscalar ");
+		logger.debug("hello {}", "world");
 	}
 
 	public static void myMethod() {
 		try {
 			throw new Exception("My Exception");
 		} catch (Exception e) {
-			//logger.error("this is an exception", e);
+			// logger.error("this is an exception", e);
 		}
 	}
 }
